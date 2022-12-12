b5721a65b5fee10651f2d654787ca44289d5ca8b
Quyen Hoang <qduhoang@gmail.com>
2022-12-12 04:11:37 -0800

    Changes to eslint - still need fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function App() {
   return (
     <div className="container">
       <div>hello world</div>
       <div>hi again world</div>
-      <div class="happy">test</div>
+      <div className="happy">test</div>
     </div>
   )
 }
